eeac6e6194bf5142d298822de93449653e6a95a7
Tom Donohue <monodot@gmail.com>
2020-07-13 13:02:58 +0100

    Complete exercise and add logging

--- README.md
+++ README.md
@@ -7,6 +7,26 @@ The tutorial uses KCL 2.2.9 to demonstrate how to send a stream of records to Ki
 [learning-kinesis]:  https://docs.aws.amazon.com/streams/latest/dev/tutorial-stock-data-kplkcl.html
 [kinesis-developer-guide]: http://docs.aws.amazon.com/kinesis/latest/dev/introduction.html
 
+## To run
+
+**Producer:** Load into an IDE and run `StockTradesWriter` class, or:
+
+```
+export AWS_PROFILE=kinesis-demo
+
+mvn exec:java -Dexec.mainClass="com.amazonaws.services.kinesis.samples.stocktrades.writer.StockTradesWriter" -Dexec.args="YourStreamName eu-west-1"
+```
+
+**Processor (Consumer):**
+
+Arguments are: _<table-name> <stream-name> <region>_:
+
+```
+export AWS_PROFILE=kinesis-demo
+
+mvn exec:java -Dexec.mainClass="com.amazonaws.services.kinesis.samples.stocktrades.writer.StockTradesProcessor" -Dexec.args="tradeytrades CillasTrades eu-west-1"
+```
+
 ## License Summary
 
 This sample code is made available under the MIT-0 license. See the LICENSE file.
--- src/main/java/com/amazonaws/services/kinesis/samples/stocktrades/processor/StockTradeRecordProcessor.java
+++ src/main/java/com/amazonaws/services/kinesis/samples/stocktrades/processor/StockTradeRecordProcessor.java
@@ -88,15 +88,24 @@ public class StockTradeRecordProcessor implements ShardRecordProcessor {
     }
 
     private void reportStats() {
-        // TODO: Implement method
+        System.out.println("****** Shard " + kinesisShardId + " stats for last 1 minute ******\n" +
+                stockStats + "\n" +
+                "****************************************************************\n");
     }
 
     private void resetStats() {
-        // TODO: Implement method
+        stockStats = new StockStats();
     }
 
     private void processRecord(KinesisClientRecord record) {
-        // TODO: Implement method
+        byte[] arr = new byte[record.data().remaining()];
+        record.data().get(arr);
+        StockTrade trade = StockTrade.fromJsonAsBytes(arr);
+        if (trade == null) {
+            log.warn("Skipping record. Unable to parse record into StockTrade. Partition Key: " + record.partitionKey());
+            return;
+        }
+        stockStats.addStockTrade(trade);
     }
 
     @Override
